c36c6d798b938d571080a314b763a59fcf5cd83f
studiosam <31368322+studiosam@users.noreply.github.com>
2018-01-20 16:08:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ bot.on('ready', function(){
     console.log('pret !');
     bot.user.setGame('vec studiosam');
 });
-
+fs.readdir("./modules/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
 
 bot.on('message', message => {
     if(message.content[0] === PREFIX){
