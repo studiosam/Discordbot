090a576493bfb456ec63f970b10c91707ee085e1
studiosam <studiosam@users.noreply.github.com>
2018-01-20 18:47:55 +0100

    1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 
 
-bot.on('ready', function(){
+client.on('ready', function(){
     console.log('pret !');
     bot.user.setGame('vec studiosam');
 });
@@ -28,18 +28,6 @@ bot.on('message', message => {
         }
       });
 
-// bot.on('guildMemberAdd', member => {
-//     member.createDM().then(channel => {
-//         return channel.send('bienvenue sur mon serveur '+ member.displayName);
-//     }).catch(console.error)
-// });
-//
-// bot.on('guildMemberRemove', member => {
-//     member.createDM().then(channel => {
-//         return channel.send('Au revoir '+ member.displayName);
-//     }).catch(console.error)
-// });
-//
 //  const embed = new Discord.RichEmbed()
 // .setTitle("Comment utiliser les long artworks")
 //
