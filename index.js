0695e6422fe718e279f69106d491d3bb55980a0d
studiosam <31368322+studiosam@users.noreply.github.com>
2018-01-20 14:31:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ bot.on('ready', function(){
 
 bot.on('message', message => {
     if(message.content[0] === PREFIX){
+        const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
         if(command === '.hello'){
             message.channel.send('```World ! ```')
