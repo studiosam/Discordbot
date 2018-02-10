4bdbf689e50094adee6f65c3be3dd8e4a4a67372
studiosam <studiosam@users.noreply.github.com>
2018-02-10 19:31:35 +0100

    1

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ client.on('message', message => {
       const guild = member.guild;
       const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
        defaultChannel.send({embed: {
-         "title": `yo ${member.mention} ${member.username+member.discriminator} bienvenue sur ${guild.name} tu est le ${guild.memberCount} a rejoindre notre serveur`,
-         "description": `${member.mention} ${member} ${member.username+member.discriminator}`,
+         "title": `yo`,
+         "description": `${member} bienvenue sur ${guild.name} tu est le ${guild.memberCount} a rejoindre notre serveur`,
         "color": 1413051,
          "image": {
            "url": "https://cdn.discordapp.com/embed/avatars/0.png"
