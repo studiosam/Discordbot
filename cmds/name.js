c092b1dc92292e576145210839736a0cce8f0e13
studiosam <studiosam@users.noreply.github.com>
2018-03-02 11:45:19 +0100

    1

--- cmds/name.js
+++ cmds/name.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-  client.setNickname({nick: message.content.replace(args[0], '')});
+  client.setNickname({nick: message.content.replace(args[0], '')}).catch(console.error(););
 /*lient.user.setUsername(args[0])
   .then(user => console.log(`My new username is ${user.username}`))
   .catch(console.error);*/
