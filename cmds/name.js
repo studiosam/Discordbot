17d3b0d0e53eeeebf0ef5da55b035696cd184922
studiosam <31368322+studiosam@users.noreply.github.com>
2018-03-02 17:33:50 +0100

    Update name.js

--- cmds/name.js
+++ cmds/name.js
@@ -1,7 +1,5 @@
 exports.run = (client, message, args) => {
-  if (client.id == 202055354861748225) {
 client.user.setUsername(args[0])
   .then(user => console.log(`My new username is ${user.username}`))
   .catch(console.error);
-}
 }
