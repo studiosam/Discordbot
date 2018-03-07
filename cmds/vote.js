71bee7d134cd29157a778c36485c26f7749dc56f
studiosam <studiosam@users.noreply.github.com>
2018-03-07 18:38:16 +0100

    1

--- cmds/vote.js
+++ cmds/vote.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 
-message.channel.send(":apple:***SONDAGE :apple:\n "+choix1+" ou "+""+choix2+"***")
+message.channel.send(":apple:")
             .then(function (message) {
               message.react("👍")
               message.react("👎")
