eb4538c9f93f7fe27b1d74bc4aca63f01d57a54a
studiosam <studiosam@users.noreply.github.com>
2018-03-07 19:18:51 +0100

    1

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('message', message => {
        }})
      });
      client.on('messageReactionAdd', (reaction, user) => {
-         if(reaction.emoji.name === ":wastebasket:") {
+         if(reaction.emoji.name === ":wastebasket:" && reaction.count() == 2) {
            reaction.message.delete()
              console.log(reaction.users);
          }
