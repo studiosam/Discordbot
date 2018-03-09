df1b91698018574a32fc0567022198f47d4f8a39
studiosam <studiosam@users.noreply.github.com>
2018-03-09 11:40:39 +0100

    1

--- index.js
+++ index.js
@@ -61,4 +61,4 @@ client.on('message', message => {
        console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
      });
 
-client.login(config.token)
+client.login(process.env.TOKEN)
