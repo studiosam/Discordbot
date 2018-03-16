4747f314c640a57aaa2155ea332596610c435030
studiosam <studiosam@users.noreply.github.com>
2018-03-16 11:36:50 +0100

    1

--- cmds/setgame.js
+++ cmds/setgame.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     client.user.setActivity(args, { type: 'PLAYING' });
-      .then(user => console.log(`new game is ${user.game}`))
+      .then(user => console.log(`new game `))
       .catch(console.error);
     console.log(client, args);
 }
