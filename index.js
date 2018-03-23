a9fde23d53f2383b38b33039e92dd90af09d6997
studiosam <studiosam@users.noreply.github.com>
2018-03-23 12:26:43 +0100

    1

--- index.js
+++ index.js
@@ -49,10 +49,9 @@ client.on('message', message => {
      );
      });
      client.on('messageReactionAdd', (reaction, user) => {
-         if(reaction.emoji.name === "💩" && reaction.count >= 2 && reaction.user.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name))) {
+         if(reaction.emoji.name === "💩" && reaction.count >= 2 && reaction.user.role.hasPermission("manageMessages") ) {
+
              reaction.message.delete(10)
-         }else {
-           console.log('non');
          }
      });
 
