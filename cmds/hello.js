05cc0265708fe7e0cdc6ec97db6e28d8ebc93321
studiosam <studiosam@users.noreply.github.com>
2018-03-23 12:07:48 +0100

    1

--- cmds/hello.js
+++ cmds/hello.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send('```World ! ```').then(msg => msg.react("💩"));
+    message.channel.send('```World ! ```').then(msg => msg.delete(1000));
     message.delete(100);
 }
