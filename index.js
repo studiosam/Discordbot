112d690218589a93be8a17b205922ad5abeb0b55
studiosam <studiosam@users.noreply.github.com>
2018-03-30 22:57:53 +0200

    1

--- index.js
+++ index.js
@@ -50,8 +50,7 @@ client.on('message', message => {
      });
      client.on('messageReactionAdd', (reaction, user, guild) => {
          if(reaction.emoji.name === "🗑" && reaction.count >= 2 ) {
-           if (reaction.message.guild.fetchMember(user).hasPermission("MANAGE_MESSAGES") == true) {
-
+           if (reaction.message.guild.fetchMember(user).hasPermission("MANAGE_MESSAGES")) {
              reaction.message.delete(10)
            }
          }
