02d6e6a2294969cc85b42f1d24a59a09c0d773aa
studiosam <studiosam@users.noreply.github.com>
2018-03-30 21:38:23 +0200

    1

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on('message', message => {
      );
      });
      client.on('messageReactionAdd', (reaction, user) => {
-         if(reaction.emoji.name === "🗑" && reaction.count >= 2 && reaction.GuildMember.hasPermission("MANAGE_MESSAGES") ) {
+         if(reaction.emoji.name === "🗑" && reaction.count >= 2 && guild.fetchMember(user).hasPermission("MANAGE_MESSAGES") ) {
              reaction.message.delete(10)
          }
      });
