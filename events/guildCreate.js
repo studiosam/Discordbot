1b3df65bd80aa6db3e75d42fbb1cbff3390f958e
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-04 17:07:25 +0200

    1

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,3 @@
-exports.run = (client, member) => {
+exports.run = (client, member,guild) => {
 console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 }
