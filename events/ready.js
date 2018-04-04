3e202895dbc41f1bfca554d09fc30a275dba64c4
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-04 18:33:54 +0200

    1

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 exports.run = (client) => {
   console.log('pret ! hhm');
-  client.user.setPresence({ game: { name: 'rien', type: 2 } });
+  client.user.setPresence({ game: { name: 'rien', type: WATCHING } });
 }
