90ec7b3c885867f62ace8d5e467d3b3a06d6b13d
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-04 17:22:46 +0200

    1

--- events/message.js
+++ events/message.js
@@ -1,15 +1,16 @@
 exports.run = (client, message) => {
+  const PREFIX = ".";
 if(message.content[0] === PREFIX){
     if (message.author.bot) return;
     if(message.content.indexOf(PREFIX) !== 0) return;
     const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
      if(message.content === '... long'){
-       let commandFile = require(`./cmds/long.js`);
+       let commandFile = require(`../cmds/long.js`);
        commandFile.run(client, message, args, Discord);
    }
     try {
-       let commandFile = require(`./cmds/${command}.js`);
+       let commandFile = require(`../cmds/${command}.js`);
        commandFile.run(client, message, args);
      } catch (err) {
      }
