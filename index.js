fdb639507a17d125d7ecfe6d307ae83c682683fb
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-04 18:18:31 +0200

    1

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({autoReconnect: true});
 
-const PREFIX = ".";
 const fs = require("fs");
-
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
     client.on(eventName, (...args) => eventFunction.run(client, ...args));
   });
 });
