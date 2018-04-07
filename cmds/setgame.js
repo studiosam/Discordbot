654e68e0739de20d1ea8407e87554653f47e3e7c
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-07 20:01:02 +0200

    1

--- cmds/setgame.js
+++ cmds/setgame.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    const games = args.split(',').join(' ')slice(1, args.length);
-    client.user.setPresence({ game: { name: `${games}`, type: 2 } });
+    const games = args.slice(1, args.length);
+    client.user.setPresence({ game: { name: `${games.split(',').join(' ')}`, type: 2 } });
 
 }
