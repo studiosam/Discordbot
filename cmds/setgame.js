1b6e9c25da4b5ec576b8a02e211e033c988c5a37
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-07 22:59:22 +0200

    1

--- cmds/setgame.js
+++ cmds/setgame.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     const games = args.slice(1, args.length).join(" ");
     const type = ["joue a", "en stream", "ecoute", "regarde"]
     message.channel.send(`${type[args[0]]} ${games}`);
-    client.user.setPresence({ game: { name: `${games}`,type: 3 }, afk: false, since: 0 })
+    client.user.setPresence({ game: { name: `${games}`,type: 3 } })
     .then(console.log)
     .catch(console.error);
 
