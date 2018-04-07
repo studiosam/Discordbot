5a58ef8848a50957e07c71124d4255a532143d96
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-07 20:51:25 +0200

    1

--- cmds/setgame.js
+++ cmds/setgame.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const games = args.slice(1, args.length).join(" ");
-    message.channel.send('Jeux defini sur :', games);
+    message.channel.send(`Jeux defini sur : ${games}`);
     client.user.setPresence({ game: { name: `${games}`, type: 2 } });
 
 }
