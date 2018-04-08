82a85c21765810ff23b3ebe5aa749de5a6c28e52
studiosam <31368322+studiosam@users.noreply.github.com>
2018-04-08 20:28:10 +0200

    1

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({autoReconnect: true});
 
 const fs = require("fs");
-var pg = require('pg');
-client.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
